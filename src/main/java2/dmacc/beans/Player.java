03ed3a1b42e9af9d567ce54c02e3439533e87f69
Clayton Blake <clay.blake9@gmail.com>
2022-04-07 16:42:39 -0500

    Added Input Validation
    Used javascript to add basic input validation to certain parts of the program.

--- src/main/java2/dmacc/beans/Player.java
+++ src/main/java2/dmacc/beans/Player.java
@@ -79,7 +79,11 @@ public class Player {
 	}
 
 	public void setNumber(int number) {
-		this.number = number;
+		if(number == 0) {
+			this.number = -1;
+		} else {
+			this.number = number;
+		}
 	}
 
 	public int getYearsPro() {
